e625be2cccc82557e03476246cda0fa7ea3f2cd6
Ander <anderson.cordova@epn.edu.ec>
2022-01-31 02:07:02 -0500

    Redirect

--- app/Http/Controllers/ForgotPasswordController.php
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -88,6 +88,6 @@ class ForgotPasswordController extends Controller
 
         DB::table('password_resets')->where(['email' => $request->email])->delete();
 
-        return redirect('/api')->with('message', 'Your password has been changed!');
+        return redirect('/')->with('message', 'Your password has been changed!');
     }
 }
