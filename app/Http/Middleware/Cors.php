f6a3a9153e2e18c5aa7f7606267c80507f2af790
Ander <anderson.cordova@epn.edu.ec>
2022-06-20 21:09:05 -0500

    Cors

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    public function handle($request, Closure $next)
+    {
+        $request->header('Access-Control-Allow-Origin', '*');
+        $request->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH,
+DELETE, OPTIONS');
+        $request->header('Access-Control-Allow-Headers', 'Content-Type,
+Authorization');
+        return $next($request);
+    }
+}
